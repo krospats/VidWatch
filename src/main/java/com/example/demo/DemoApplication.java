8829c674f3d0978c05d3a75696b6fa5187da841f
krospats <krospats@gmail.com>
2025-02-17 21:27:12 +0300

    test thing

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,10 +3,13 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SuppressWarnings("checkstyle:MissingJavadocType")
 @SpringBootApplication
 public class DemoApplication {
-
-	public static void main(String[] args) {
+ //TEST THING
+	@SuppressWarnings({"checkstyle:FileTabCharacter", "checkstyle:Indentation",
+			"checkstyle:MissingJavadocMethod"})
+    public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
