fa143d7db57ab1cdd0664689ba9bb87a59159aa6
krospats <krospats@gmail.com>
2025-02-18 11:16:41 +0300

    test thing

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
-
+    //hello
     @GetMapping("/hello")
     public String sayHello() {
         return "Hello, Spring Boot!";
