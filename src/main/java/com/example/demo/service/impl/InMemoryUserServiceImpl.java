b7eada111067fbc484bb924209f285cd1c3ac85a
krospats <krospats@gmail.com>
2025-02-28 02:14:50 +0300

    small changes

--- src/main/java/com/example/demo/service/impl/InMemoryUserServiceImpl.java
+++ src/main/java/com/example/demo/service/impl/InMemoryUserServiceImpl.java
@@ -5,12 +5,15 @@ import com.example.demo.repository.InMemoryStudentDao;
 import com.example.demo.service.UserService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 @AllArgsConstructor
 public class InMemoryUserServiceImpl implements UserService {
 
+
+    @Autowired
     private final InMemoryStudentDao studentRepository;
 
     @Override
