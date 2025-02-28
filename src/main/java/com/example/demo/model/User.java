18d758de5cecfa1676ad6e7f846da4516dc98e9c
krospats <krospats@gmail.com>
2025-02-28 11:48:51 +0300

    small changes

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -2,9 +2,11 @@ package com.example.demo.model;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 
 @Data
 @Builder
+@Getter
 public class User {
 
     private String userName;
