f4625b2390dd298732537ae0f962e98f235c0491
krospats <krospats@gmail.com>
2025-02-28 11:29:49 +0300

    small changes

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -4,6 +4,8 @@ import com.example.demo.model.User;
 import com.example.demo.service.UserService;
 import java.util.List;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1/users")
 @AllArgsConstructor
 public class UserController {
+
 
     private final UserService userService;
 
--- src/main/java/com/example/demo/service/UserService.java
+++ src/main/java/com/example/demo/service/UserService.java
@@ -1,14 +1,31 @@
 package com.example.demo.service;
 
 import com.example.demo.model.User;
+import com.example.demo.repository.InMemoryStudentDao;
 import java.util.List;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
 
-public interface UserService {
-    List<User> findAllUsers();
 
-    User findUserByUserName(String userName);
 
-    User findUserByUserEmail(String email);
+
+@Service
+@AllArgsConstructor
+public class UserService {
+    private final InMemoryStudentDao studentRepository;
+
+    public List<User> findAllUsers() {
+        return studentRepository.findAllUsers();
+    }
+
+    public User findUserByUserName(String userName) {
+        return studentRepository.findUserByUserName(userName);
+    }
+
+    public User findUserByUserEmail(String email) {
+        return studentRepository.findUserByUserEmail(email);
+
+    }
 
 
 }
