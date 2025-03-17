27ea842988f2dc131ab546152b203b0237b87822
krospats <krospats@gmail.com>
2025-03-18 01:16:33 +0300

    refactoring

--- src/main/java/com/example/demo/model/Video.java
+++ src/main/java/com/example/demo/model/Video.java
@@ -18,7 +18,6 @@ import lombok.Setter;
 @Getter
 @Setter
 public class Video {
-    public Video() {}
 
     @Id // Указываем, что это первичный ключ
     @GeneratedValue(strategy = GenerationType.IDENTITY) // Автоматическая генерация значения
